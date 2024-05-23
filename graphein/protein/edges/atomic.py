f2ae561b144cacfac54a6965b88d856fb6d0005f
Masoud Ramuz <masoud.ramuz@gmail.com>
2024-05-23 16:19:02 +0200

    Added HSP (protonated histidine) amino acid type to resi_atoms.py

--- graphein/protein/edges/atomic.py
+++ graphein/protein/edges/atomic.py
@@ -209,7 +209,6 @@ def add_bond_order(G: nx.Graph) -> nx.Graph:
     :rtype: mx.Graph
     """
     for u, v, a in G.edges(data=True):
-        print("G.nodes[u]: ", G.nodes[u])
         atom_a = G.nodes[u]["element_symbol"]
         atom_b = G.nodes[v]["element_symbol"]
 
